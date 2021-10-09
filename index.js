2a78a9912674dacdaba32166aab676064cf3dede
NuestroAmor <92233017+NuestroAmor@users.noreply.github.com>
2021-10-09 18:35:18 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function amor(){
+    if( document.getElementById("Usuario").value == "vivi"){
+        if(document.getElementById("contrasena").value == "prueba123"){
+
+        }else{
+            alert("contrasena incorrecta");
+        }
+    }else{
+        alert("Usuario incorrecta");
+    }
+}
